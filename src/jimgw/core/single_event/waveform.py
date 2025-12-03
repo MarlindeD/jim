77e864e97c7d84dc6998dcb50e86c22137d5f520
Marlinde Drent <mdrent@int5.local.snellius.surf.nl>
2025-12-03 14:59:27 +0100

    turn off import

--- src/jimgw/core/single_event/waveform.py
+++ src/jimgw/core/single_event/waveform.py
@@ -7,7 +7,7 @@ from ripplegw.waveforms.IMRPhenomPv2 import gen_IMRPhenomPv2_hphc
 from ripplegw.waveforms.TaylorF2 import gen_TaylorF2_hphc
 from ripplegw.waveforms.IMRPhenomD_NRTidalv2 import gen_IMRPhenomD_NRTidalv2_hphc
 from ripplegw.waveforms.IMRPhenomXAS import gen_IMRPhenomXAS_hphc
-from ripplegw.waveforms.IMRPhenomXAS_NRTidalv3 import gen_IMRPhenomXAS_NRTidalv3_hphc
+#from ripplegw.waveforms.IMRPhenomXAS_NRTidalv3 import gen_IMRPhenomXAS_NRTidalv3_hphc
 from ripplegw.waveforms.TaylorF2QM_taper import gen_TaylorF2QM_hphc
 
 
